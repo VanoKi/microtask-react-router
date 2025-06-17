268922dee528bf7da30ffd6502438d6a36389c64
VanoKi <kirsvano@gmail.com>
2025-06-17 18:38:24 +0300

    fix: path

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import {PageTwo} from "./components/pages/PageTwo.tsx";
 import {PageThree} from "./components/pages/PageThree.tsx";
 import {Error404} from "./components/pages/Error404.tsx";
 
+const PATH = {
+  PAGE1: 'page1',
+  PAGE2: 'page2',
+  PAGE3: 'page3',
+  ERROR: 'error404'
+} as const
+
 export const App = ()=> {
   const getClassName = ({isActive} : {isActive:boolean}) => {
     return isActive ? styles.activeNavLink : styles.navLink
@@ -15,20 +22,20 @@ export const App = ()=> {
       <div className={styles.header}><h1>HEADER</h1></div>
       <div className={styles.body}>
         <div className={styles.nav}>
-          <div> <NavLink to={'/page_one'} className={getClassName}> Adidas </NavLink></div>
-          <div> <NavLink to={'/page_two'} className={getClassName}> Puma </NavLink></div>
-          <div> <NavLink to={'/page_three'} className={getClassName}> Abibas </NavLink></div>
+          <div> <NavLink to={PATH.PAGE1} className={getClassName}> Adidas </NavLink></div>
+          <div> <NavLink to={PATH.PAGE2} className={getClassName}> Puma </NavLink></div>
+          <div> <NavLink to={PATH.PAGE3} className={getClassName}> Abibas </NavLink></div>
         </div>
         <div className={styles.content}>
           {/*Здесь будут кроссовки*/}
           <Routes>
             <Route  path={'/'} element={<Navigate to={'/page_one'}/> }/>
-            <Route path={'/page_one'} element={<PageOne/>}/>
-            <Route path={'/page_two'} element={<PageTwo/>}/>
-            <Route path={'/page_three'} element={<PageThree/>}/>
+            <Route path={PATH.PAGE1} element={<PageOne/>}/>
+            <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+            <Route path={PATH.PAGE3} element={<PageThree/>}/>
 
-            <Route path={'/error404'} element={<Error404/>}/>
-            <Route path={'*'} element={<Navigate to={'/error404'}/>}/>
+            <Route path={PATH.ERROR} element={<Error404/>}/>
+            <Route path={'*'} element={<Navigate to={PATH.ERROR}/>}/>
           </Routes>
         </div>
       </div>
