b4bc3d28b9125f2c51bd56d3fa103d9863556154
VanoKi <kirsvano@gmail.com>
2025-06-17 19:26:58 +0300

    fix: rename components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import styles from "./components/Site.module.css";
 import {Link, Navigate, NavLink, Route, Routes} from "react-router-dom";
-import {PageOne} from "./components/pages/PageOne.tsx";
-import {PageTwo} from "./components/pages/PageTwo.tsx";
-import {PageThree} from "./components/pages/PageThree.tsx";
+import {PageOne} from "./components/pages/Adidas.tsx";
+import {Puma} from "./components/pages/Puma.tsx";
+import {Abibas} from "./components/pages/Abibas.tsx";
 import {Error404} from "./components/pages/Error404.tsx";
 
 const PATH = {
-  PAGE1: '/page1',
-  PAGE2: '/page2',
-  PAGE3: '/page3',
+  PAGE1: '/adidas',
+  PAGE2: '/puma',
+  PAGE3: '/abibas',
   ERROR: '/error404'
 } as const
 
@@ -31,8 +31,8 @@ export const App = ()=> {
           <Routes>
             <Route  path={'/'} element={<Navigate to={PATH.PAGE1}/> }/>
             <Route path={PATH.PAGE1} element={<PageOne/>}/>
-            <Route path={PATH.PAGE2} element={<PageTwo/>}/>
-            <Route path={PATH.PAGE3} element={<PageThree/>}/>
+            <Route path={PATH.PAGE2} element={<Puma/>}/>
+            <Route path={PATH.PAGE3} element={<Abibas/>}/>
 
             <Route path={PATH.ERROR} element={<Error404/>}/>
             <Route path={'*'} element={<Navigate to={PATH.ERROR}/>}/>
